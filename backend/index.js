7a034f27debc6077c1899e0f99a254e206b9743e
Ramon Tabilin <>
2023-05-19 13:23:11 -0400

    Cookie fix for real

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
 app.use(express.json({ limit: '50mb' }))
 app.use(cors({
 	origin: process.env.BASE_URL,
+	credentials: true,
 }))
 app.use(cookieParser())
 
--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,5 +3,6 @@ import axios from "axios"
 export const makeRequest = axios.create({
 	baseURL: import.meta.env.VITE_BASE_URL,
 	withCredentials: true,
+	credentials: 'include',
 	headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
 })
